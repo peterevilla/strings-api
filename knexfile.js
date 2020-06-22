10a9d57edac7d31fc200549b2d4e74f093548334
Pedro Revilla <peterevilla@gmail.com>
2020-06-21 20:38:49 -0500

    fix

--- knexfile.js
+++ knexfile.js
@@ -22,7 +22,7 @@ module.exports = {
   
 production: {
   client: 'pg',
-  connection: process.env.DATABASE_URL,
+  connection: pgConnection,
   migrations: {
     directory: './database/migrations'
   },
